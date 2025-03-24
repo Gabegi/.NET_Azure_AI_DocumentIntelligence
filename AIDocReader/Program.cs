418e3329b17ea0833260b534a0bf69abde6d774c
Gabegi <gabriel_pirastru@hotmail.fr>
2025-03-24 10:19:47 +0100

    added post api

--- AIDocReader/Program.cs
+++ AIDocReader/Program.cs
@@ -1,34 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.MapPost("/getword", (Keyword request) =>
+{
+    var response = new { Keyword = $"Received: {request.Word}" };
+    return Results.Json(response);
+});
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
-
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+public class Keyword
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public string Word { get; set; }
 }
