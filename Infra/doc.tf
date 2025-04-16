fd6d86969e6bd521dd25416be712f296df8e3d8d
Gabegi <gabriel_pirastru@hotmail.fr>
2025-04-16 16:32:06 +0400

    doc ai

--- Infra/doc.tf
+++ Infra/doc.tf
@@ -1,13 +1,7 @@
-resource "azurerm_cognitive_account" "docintelligence_account" {
-  name                = var.account_name
-  location            = azurerm_resource_group.docintelligence_rg.location
-  resource_group_name = azurerm_resource_group.docintelligence_rg.name
+resource "azurerm_cognitive_account" "doc-ai" {
+  name                = "testdfdfadsfaeafd"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   kind                = "DocumentIntelligence"
   sku_name            = "F0"
-
-  properties {
-    network_acls {
-      default_action = "Allow"
-    }
-  }
 }
